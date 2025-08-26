9b79294bdbb29bb78a01d55e0ff2bbec10b579c6
nirbhaysingh2572 <chhivalaha499@gmail.com>
2025-08-26 17:00:42 +0530

    adding env variable

---
+++ flightSearchService/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const { PORT } = require('./config/serverConfig')
+
+const  setupAndStartServer = async () => {
+
+    //creating the express object
+    const app = express();
+    app.listen(PORT, () => {
+        console.log(`server started at ${PORT}`);
+    })
+
+
+}
+
+setupAndStartServer();
+ 
+
