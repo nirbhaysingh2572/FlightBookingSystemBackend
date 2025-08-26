ed6d5c70a5fcfcc2114f831ffc38b2e9c1e76187
nirbhaysingh2572 <chhivalaha499@gmail.com>
2025-08-26 17:16:33 +0530

    addng body parser

--- flightSearchService/src/index.js
+++ flightSearchService/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 
 const { PORT } = require('./config/serverConfig')
 
@@ -6,6 +8,10 @@ const  setupAndStartServer = async () => {
 
     //creating the express object
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
     app.listen(PORT, () => {
         console.log(`server started at ${PORT}`);
     })
